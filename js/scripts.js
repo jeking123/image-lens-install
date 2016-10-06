cfe388cea59c7fc9a808c127a46451662cd1c466
jeking123 <jking47@vcd-ch204-10.eastern.ewu.edu>
2016-10-06 14:17:51 -0700

    Installed

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//Test scripts.js file
+console.log('okayUSA');
+
+//Test Jquery
+//$('img').hide();
+
+$('img').imageLens({
+    imageSrc: "/img/watch-zoom.jpg",
+    lensSize: 600,
+    borderColor: 'yellow'
+    
+});
